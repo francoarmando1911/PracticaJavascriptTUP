75ab5ea2cdd6882e34c595c07237fa3f42d0d493
Franco Armando <francoarmando1911@gmail.com>
2025-08-15 07:57:02 -0300

    update ejercicio 3 y 4

---
+++ Javascript/ejercicio4/ejercicio4.js
@@ -0,0 +1,9 @@
+let valor1 = 30
+let valor2 = 50
+console.log("Los valores sin intercambiar son: ", valor1, " y ", valor2)
+
+let varX = valor1
+valor1 = valor2
+valor2 = varX
+
+console.log("Los valores de las variables ya intercambiadas son : ", valor1, " y ", valor2)
